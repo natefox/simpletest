0e8208e4518b5dfbc4cd15769ca714a5c7441d91
Nate Fox <nate.m.fox@gmail.com>
2016-01-02 22:35:36 -0800

    updates

--- 1-elasticsearch/run.sh
+++ 1-elasticsearch/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rm -f es
+docker rm -f es > /dev/null 2>&1
 time docker run -d -p 9200:9200 --name es natefoxcorpinfo/elasticsearch
--- 1-nginx/run.sh
+++ 1-nginx/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rm -f nginx
+docker rm -f nginx >/dev/null 2>&1
 time docker run -d -p 80:80 --name nginx --link es:elasticsearch natefoxcorpinfo/nginx
